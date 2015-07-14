4c1b5c9528cc0a04f746f99360eb2fc25da2e4ae
Erich Quintero <qbantek@gmail.com>
2015-07-14 12:55:55 -0400

    init

---
+++ lib/less/rails/jasny/bootstrap.rb
@@ -0,0 +1,11 @@
+require "less/rails/jasny/bootstrap/version"
+
+module Less
+  module Rails
+    module Jasny
+      module Bootstrap
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/less/rails/jasny/bootstrap/version.rb
@@ -0,0 +1,9 @@
+module Less
+  module Rails
+    module Jasny
+      module Bootstrap
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
